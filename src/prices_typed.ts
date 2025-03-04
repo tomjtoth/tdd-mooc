10e2855f440ca696fc0025a80e5791d62c1f93e7
Tamás Tóth <tomjtoth@gmail.com>
2025-03-04 19:55:20 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -27,6 +27,7 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
+    const dateTemp = convert(date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
