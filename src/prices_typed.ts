ce252d9ea274512214eaa4a50936973a0b88370f
Tamás Tóth <tomjtoth@gmail.com>
2025-03-04 19:50:55 +0200

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -39,7 +39,7 @@ function createApp(database: Database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, convert(date), baseCost);
     }
   }
 
